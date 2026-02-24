d5d382ec1a5d89530547a769f093f84c38163d75
Tobias Köngeter <tobias.koengeter@bitandblack.com>
2026-02-24 13:06:33 +0100

    Add empty payload

---
+++ src/Api/EmptyPayload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Bit&Black Hyphenizer SDK.
+ *
+ * @author Tobias Köngeter
+ * @copyright Copyright © Bit&Black
+ * @link https://www.bitandblack.com
+ * @license MIT
+ */
+
+namespace BitAndBlack\Hyphenizer\Sdk\Api;
+
+readonly class EmptyPayload implements PayloadInterface
+{
+    /**
+     * @return array<empty>
+     */
+    public function jsonSerialize(): array
+    {
+        return [];
+    }
+}
